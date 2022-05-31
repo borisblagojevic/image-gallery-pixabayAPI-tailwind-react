b8c40bcb83951da3cd820fefdbdd18a6b8deb8aa
Boris Blagojevic <33908472+Blagoja95@users.noreply.github.com>
2022-05-31 11:14:14 +0200

    tailwind setup fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 function App() {
   return (
     <div class="max-w-sm rounded overflow-hidden shadow-lg">
-      <img src="https://source.unsplash.com/random" alt="" className="w-50" />
+      {/* <img src="https://source.unsplash.com/random" alt="" className="w-50" /> */}
+      <h1 className="text-sm font-bold underline text-red-500">Hello world!</h1>
     </div>
   );
 }
