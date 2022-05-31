9c76b2fec7628a8ee8b072686fee95ae202ef63d
Boris Blagojevic <33908472+Blagoja95@users.noreply.github.com>
2022-05-31 09:17:33 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div class="max-w-sm rounded overflow-hidden shadow-lg">
+      <img src="https://source.unsplash.com/random" alt="" className="w-50" />
+    </div>
+  );
+}
+
+export default App;
